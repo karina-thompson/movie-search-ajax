1c82d5c952726addc546a06fbbe3c1e721247538
Karina Thompson <klthompsn8@gmail.com>
2016-05-03 15:35:02 +1000

    movie search works

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+var listMovies = function(data) {
+  $('p').detach();
+  data["Search"].forEach(function(movie) {
+    var $newListing = $('<p>').text(movie["Title"]);
+    $('.results').append($newListing);
+  })
+}
+
+
+var searchMovies = function() { $.ajax({
+  url: 'http://www.omdbapi.com/?s=' + $('.movieSearch').val() 
+}).done(listMovies);
+};
+  
+  
+$('.search').on('click', searchMovies);
